8fa1abca2c2bb598a9cb2fab8a95da7bcdb67a32
Darren <491361994@qq.com>
2018-11-12 19:18:52 +0800

    first commit

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,8 +34,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      'vue': 'vue/dist/vue.js',
       '@': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'base': resolve('src/base'),
+      'api': resolve('src/api')
     }
   },
   module: {
