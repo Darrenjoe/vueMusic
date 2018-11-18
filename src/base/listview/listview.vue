30f565c513897205ca14655222164b8d3912d4d4
Darren <491361994@qq.com>
2018-11-18 23:52:59 +0800

    first commit

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -9,7 +9,10 @@
       <li v-for="(group, index) in data" class="list-group" ref="listGroup" :key="index">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
-          <li v-for="(item, index) in group.items" class="list-group-item" :key="index">
+          <li v-for="(item, index) in group.items"
+              class="list-group-item"
+              :key="index"
+              @click="selectItem(item)">
             <img class="avatar" v-lazy="item.avatar">
             <span class="name">{{item.name}}</span>
           </li>
@@ -76,6 +79,9 @@ export default {
     }
   },
   methods: {
+    selectItem(item) {
+      this.$emit('select', item)
+    },
     onShortcutTouchStart(e) {
       let anchorIndex = getData(e.target, 'index')
       let firstTouch = e.touches[0]
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-    <listview :data="singers"></listview>
+    <listview @select="selectSinger" :data="singers"></listview>
   </div>
 </template>
 
@@ -23,6 +23,11 @@ export default {
     this._getSingerList()
   },
   methods: {
+    selectSinger(singer) {
+      this.$router.push({
+        path: `/singer/${singer.id}`
+      })
+    },
     _getSingerList() {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
